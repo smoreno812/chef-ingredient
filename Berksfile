29a896016b2491bb45be465a0a369aa1b980b343
Stephen Moreno <morste02@CSGICORP.COM>
2017-01-10 10:06:13 -0600

    first

---
+++ Berksfile
@@ -0,0 +1,13 @@
+source 'https://supermarket.chef.io'
+
+metadata
+
+cookbook 'compat_resource', '>= 12.10'
+
+
+
+
+group :integration do
+  cookbook 'git'
+  cookbook 'apt'
+end
---
+++ metadata.rb
@@ -0,0 +1,15 @@
+name 'csg_chef_ingredient'
+version '0.21.0'
+maintainer 'Chef Software, Inc.'
+maintainer_email 'cookbooks@chef.io'
+license 'Apache 2.0'
+description 'Primitives for managing Chef products and packages'
+
+depends 'compat_resource', '>= 12.10'
+
+
+
+chef_version '>= 12.1'
+
+issues_url       'https://jira.csgicorp.com/projects/AA/issues'
+source_url		 'http://stash.csgsystems.com/projects/ASA/repos/chefinfra/browse/Chef'
